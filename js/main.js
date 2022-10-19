e13bb2210730513198679b7e02033fb771e49fdd
Dan-k391 <wang.yifan@ulink.cn>
2022-10-20 00:34:45 +0800

    add the load example function

--- js/main.js
+++ js/main.js
@@ -19,9 +19,79 @@ var app = new Vue({
         markers: [],
         cmd: '',
         do_input: false,
-        input_data: ''
+        input_data: '',
+        example_id: '',
+        example_names: {
+            '0': 'Hello World',
+            '1': 'Linked list',
+        },
+        example_code: {
+            '0': `OUTPUT "Hello World!"`,
+            '1': `TYPE ListNode
+    DECLARE Data: REAL
+    DECLARE Ptr: INTEGER
+ENDTYPE
+
+DECLARE StartPtr: INTEGER
+DECLARE FreePtr: INTEGER
+
+DECLARE List: ARRAY[0:6] OF ListNode
+
+PROCEDURE InitialiseList()
+    DECLARE Index: INTEGER
+    StartPtr <- -1
+    FreePtr <- 0
+    FOR Index <- 0 TO 5
+        List[Index].Ptr <- Index + 1
+    NEXT Index
+    List[6].Ptr <- -1
+ENDPROCEDURE
+
+PROCEDURE InsertAtHead(DataItem: REAL)
+    DECLARE NewNode: INTEGER
+    IF FreePtr = -1 THEN
+        OUTPUT "Memory is used out"
+    ELSE
+        NewNode <- FreePtr
+        FreePtr <- List[FreePtr].Ptr
+        List[NewNode].Data <- DataItem
+        List[NewNode].Ptr <- StartPtr
+        StartPtr <- NewNode
+    ENDIF
+ENDPROCEDURE
+
+PROCEDURE PrintList()
+    DECLARE CurrentPtr: INTEGER
+    CurrentPtr <- StartPtr
+    WHILE CurrentPtr <> -1
+        OUTPUT List[CurrentPtr].Data
+        CurrentPtr <- List[CurrentPtr].Ptr
+    ENDWHILE
+ENDPROCEDURE
+
+// Call the initial procedure
+CALL InitialiseList()
+
+// For for loop to test
+DECLARE i: INTEGER
+
+FOR i <- 0 TO 7
+    CALL InsertAtHead(RND())
+NEXT i
+
+CALL PrintList()
+        `
+        }
     },
     mounted() {
+        // window.onbeforeunload = e => {
+        //     e = e || window.event
+        //     if (e) {
+        //         e.returnValue = '关闭提示'
+        //     }
+        //     return '关闭提示'
+        // }
+
         require.config({ paths: { 'vs': 'https://cdn.bootcdn.net/ajax/libs/monaco-editor/0.23.0/min/vs' } });
         window.MonacoEnvironment = {
             getWorkerUrl: function (workerId, label) {
@@ -195,10 +265,15 @@ var app = new Vue({
                 this.input_data = this.cmd;
             }
             else {
+                let _this = this;
                 if (this.cmd == 'help') {
                     this.terminal.writeln('help: show help');
                     this.terminal.writeln('run: run the code');
                     this.terminal.writeln('clear/cls: clear the terminal');
+                    this.terminal.writeln('load: load the following examples');
+                    for (let key in _this.example_names) {
+                        this.terminal.writeln(key + ': ' + _this.example_names[key]);
+                    }
                     this.terminal.write(this.prefix);
                 }
                 else if (this.cmd == 'run') {
@@ -212,6 +287,15 @@ var app = new Vue({
                     this.terminal.writeln('An egg? Maybe...');
                     this.terminal.write(this.prefix);
                     console.log('......');
+                }
+                else if (this.cmd.substring(0, 4) == 'load') {
+                    // number is a string
+                    let number = this.cmd.substring(5);
+                    console.log(number);
+                    if (number in _this.example_code) {
+                        this.editor.setValue(_this.example_code[number]);
+                    }
+                    this.terminal.write(this.prefix);
                 }
                 else {
                     this.terminal.writeln(this.cmd + ': Command not found');
